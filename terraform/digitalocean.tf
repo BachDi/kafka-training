6851912971770b6ec201455a0f4039bae77ee617
Vu Hoang Phuc <vuhoangphuc@udt.group>
2023-02-19 11:12:56 +0700

    Test

---
+++ terraform/digitalocean.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.26.0"
+    }
+  }
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_public_key" {
+  description = "Public key to connect to instance through SSH"
+}
+
+variable "ssh_private_key" {
+  description = "Private key to connect to instance through SSH"
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "terraform-training" {
+  image  = "ubuntu-20-04-x64"
+  name   = "terraform-training"
+  region = "sgp1"
+  size   = "s-1vcpu-2gb"
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = var.ssh_private_key
+    host        = self.ipv4_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ~/.ssh",
+      "echo '${var.ssh_public_key}' >> ~/.ssh/authorized_keys",
+      "chmod 700 ~/.ssh",
+      "chmod 600 ~/.ssh/authorized_keys",
+      "service ssh restart",
+    ]
+  } 
+}
