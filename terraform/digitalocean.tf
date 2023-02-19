03ecadfed2c59fcc8229671a25681047bddcc760
Vu Hoang Phuc <vuhoangphuc@udt.group>
2023-02-19 12:07:10 +0700

    1

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -23,11 +23,17 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+resource "digitalocean_ssh_key" "default" {
+  name       = "DO Default SSH Key"
+  public_key = var.ssh_public_key
+}
+
 resource "digitalocean_droplet" "terraform-training" {
   image  = "ubuntu-20-04-x64"
   name   = "terraform-training"
   region = "sgp1"
   size   = "s-1vcpu-2gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 
   connection {
     type        = "ssh"
